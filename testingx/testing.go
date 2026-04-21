c700248b5fdf588bb424bcf2358564208c0e6fa8
Nishad Wadwekar <nishad.wadwekar@gmail.com>
2026-04-21 20:24:13 +0530

    testing utils

---
+++ testingx/testing.go
@@ -0,0 +1,79 @@
+// Package testingx
+package testingx
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+var errMatcher func(err error, want any) bool = func(err error, want any) bool {
+	return errors.Is(err, want.(error))
+}
+
+func SetDefaultErrMatcher(f func(err error, want any) bool) {
+	errMatcher = f
+}
+
+func TypeMismatch(t testing.TB, got, want any) {
+	t.Helper()
+	t.Errorf("got type %T, want %T", got, want)
+}
+
+func ErrUnexpected(t testing.TB, got error) {
+	t.Helper()
+	t.Errorf("unexpected error: %v", got)
+}
+
+func ErrExpected(t testing.TB, want any) {
+	t.Helper()
+	t.Errorf("expected error %v, got nil", want)
+}
+
+func ErrMismatch(t testing.TB, got error, want any) {
+	t.Helper()
+	t.Errorf("got error %v, want error %v", got, want)
+}
+
+func Mismatch(t testing.TB, diff string) {
+	t.Helper()
+	t.Errorf("mismatch (-want +got):\n%s", diff)
+}
+
+func Check(t testing.TB, want, got any, opts ...cmp.Option) {
+	t.Helper()
+	if diff := cmp.Diff(want, got, opts...); diff != "" {
+		Mismatch(t, diff)
+	}
+}
+
+func CheckErr(t testing.TB, got, want any, gotErr error, wantErr any, opts ...cmp.Option) {
+	t.Helper()
+	switch {
+	case gotErr != nil && wantErr != nil:
+		if !errMatcher(gotErr, wantErr) {
+			ErrMismatch(t, gotErr, wantErr)
+		}
+	case gotErr != nil && wantErr == nil:
+		ErrUnexpected(t, gotErr)
+	case gotErr == nil && wantErr != nil:
+		ErrExpected(t, wantErr)
+	case gotErr == nil && wantErr == nil:
+		Check(t, got, want, opts...)
+	}
+}
+
+func CheckPanic(t testing.TB, fn func(), expectedPanic any) (panicked bool) {
+	t.Helper()
+	defer func() {
+		if r := recover(); r != nil {
+			if r != expectedPanic {
+				panic(r)
+			}
+			panicked = true
+		}
+	}()
+	fn()
+	return
+}
